12150c988488c39582d8c33dfcfa111ed8d27847
Javier Arauz <84407957+Javier0929@users.noreply.github.com>
2022-12-30 19:18:30 -0500

    Microservicios

---
+++ TRABAJOS-COMPLEMENTARIOS/Parcial2-Taller2-Semana12/database/config.js
@@ -0,0 +1,20 @@
+//Base de datos mongoose 
+const  mongoose =  require('mongoose');
+
+
+const dbConnection = async ()=>{
+
+    try{
+   
+        await mongoose.connect( process.env.MONGODB_CNN);
+        console.log('Base de datos escuchando')
+    }
+    catch(error){
+        console.log(error);
+        throw new Error('Error al conectarse con la base de datos')
+    }
+
+}
+module.exports = {
+    dbConnection
+}
